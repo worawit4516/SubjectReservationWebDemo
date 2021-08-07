8209052a0309ff236803e2d2ab97ab1ebd2b787d
Earty4516 <worawit.4516@mail.kmutt.ac.th>
2021-08-07 14:18:30 +0700

    update msg

--- subject-reservation-web-demo/src/components/HelloWorld.vue
+++ subject-reservation-web-demo/src/components/HelloWorld.vue
@@ -12,7 +12,7 @@
 
       <v-col class="mb-4">
         <h1 class="display-2 font-weight-bold mb-3">
-          Welcome to Vuetify
+          Welcome to Vuetify new message
         </h1>
 
         <p class="subheading font-weight-regular">
@@ -94,7 +94,6 @@
 <script>
   export default {
     name: 'HelloWorld',
-
     data: () => ({
       ecosystem: [
         {
